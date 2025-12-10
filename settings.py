18e3994680a7bf9c11c3cfe62a9c3366b669e17b
Aleksey <aleksey-000@mail.ru>
2025-12-10 19:45:29 +0300

    файл конфигурации

---
+++ settings.py
@@ -0,0 +1,26 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+# Файл содержит настройки или перменные среды для использования в проекте
+
+# Yandex App API
+YAPP_TOKEN = os.getenv('yapp_token')
+
+# База данных
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+# Minio S3 хранилище
+ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+SECRET_KEY = os.getenv('S3_SECRET_KEY')
+# особо важный параметр при развертывании
+BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+MINIO_SECURE = os.getenv('S3_MINIO_SECURE')
+OUTER_ENDPOINT_URL = os.getenv('S3_OUTER_ENDPOINT_URL')
