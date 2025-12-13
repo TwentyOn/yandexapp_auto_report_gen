6d676447d316d107602388db666884bf25a1be54
Aleksey <aleksey-000@mail.ru>
2025-12-13 13:06:25 +0300

    подготовка в внедрению скрипта (см прошлый коммит)

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 url_params = {
     '703986845': 'utm_source=yandex&utm_medium=mospaynew&utm_campaign={campaign_id}&utm_content={ad_id}&utm_term={keyword}',
     '703723040': None,
@@ -8,8 +10,22 @@ url_params = {
     '702498562': None
 }
 
-for campaign_id in params:
-    if params[campaign_id]:
-        result.append(next(filter(lambda param: '{campaign_id}' in param, params[campaign_id].split('&'))))
+result = []
 
-print(result)
+for campaign_id in url_params:
+    if url_params[campaign_id]:
+        param = next(filter(lambda param: '{campaign_id}' in param, url_params[campaign_id].split('&')))
+        result.append((campaign_id, param))
+    else:
+        result.append((campaign_id, 'utm_campaign={campaign_id}'))
+
+result = map(lambda t: (t[0], t[1].split('=')[0]), result)
+
+df = pd.DataFrame(result, columns=['campaign_id', 'param'])
+df = df.groupby('param')['campaign_id'].apply(list).to_dict()
+d = "ym:ts:urlParameter{'{{URL_PARAM}}'}"
+for url_param in df:
+    ids = df[url_param]
+    dimension = d.replace('{{URL_PARAM}}', url_param)
+    print(ids)
+    print(dimension)
